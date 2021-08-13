f17bb096553f1775db9ad55cc2e8441e7ff7ff3d
gobborg <kotatsugoblin@gmail.com>
2021-08-13 17:55:09 +0900

    the counter works

--- td.js
+++ td.js
@@ -1,7 +1,14 @@
-function countChars(obj){
+console.log('cup');
+var twitterText = require('twitter-text');
+
+global.countChars = function countChars(){
+
+    var thingOne = document.getElementById("tweet").value;
+    var content = twitterText.parseTweet(thingOne);
     var maxLength = 280;
-    var strLength = obj.value.length;
-    
+    //var strLength = document.getElementById("tweet").value.length;   
+    var strLength = content.weightedLength;
+
     if(strLength > maxLength){
         document.getElementById("charNum").innerHTML = '<span style="color: red;">'+strLength+' out of '+maxLength+' characters</span>';
     }else{
