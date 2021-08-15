a23bb680291a9ef6aa5411980046ab3e28bf2e00
gobborg <kotatsugoblin@gmail.com>
2021-08-15 19:43:20 +0900

    clean up

--- td.js
+++ td.js
@@ -36,25 +36,3 @@ remove = function remove() {
 
 $('.add').on('click', add);
 $('.remove').on('click', remove);
-
-/*
-//begin c/p'd code//
-function add() {
-  var new_chq_no = parseInt($('#total_chq').val()) + 1;
-  var new_input = "<input type='text' id='new_" + new_chq_no + "'>";
-
-  $('#new_chq').append(new_input);
-
-  $('#total_chq').val(new_chq_no);
-}
-
-function remove() {
-  var last_chq_no = $('#total_chq').val();
-
-  if (last_chq_no > 1) {
-    $('#new_' + last_chq_no).remove();
-    $('#total_chq').val(last_chq_no - 1);
-  }
-}
-//end c/p'd code//
-*/
